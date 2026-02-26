35b78bef2c03b2f079dd44720eaff648b8182a2e
Lunarmist-byte <amal.s.kumar2005@gmail.com>
2026-02-26 09:19:29 +0530

    finishing up main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from nerf_trainer import NeRFTrainer
 from bridge_converter import NeRFToGaussianBridge
 from HybridCoOptimizer import HybridCoOptimizer
 from rasterizer import RoomRasterizerCUDA
+from gaussian_model import GaussianModel
 
 def main():
     print("Starting 3D Reconstruction")
@@ -19,3 +20,17 @@ def main():
     trainer=NeRFTrainer(nerf)
     #Pre-Train
     print("warming up NeRF")
+    for epoch in range(10):
+        for i in range(len(dataset.images)):
+            img,poses=dataset.get_training_batch(i)
+            loss=trainer.train_step(img,poses)
+        print(f"NeRF Epoch {epoch} Loss:{loss}")
+    #Bridge
+    print("Seeding Gaussians")
+    bbox=[[-5,-5,-5],[5,5,5]]#example bounds
+    bridge=NeRFToGaussianBridge(nerf,bbox)
+    init_xyz,init_rgb=bridge.generate_initial_gaussians()
+
+
+
+
